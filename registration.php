97572f3eeabdd9c4012b8c5b617eb750832d8211
Jandel Isidro <kazutokirigayakirito60@gmail.com>
2022-04-12 12:03:34 +0800

    8th minor Commit

--- registration.php
+++ registration.php
@@ -1,15 +1,25 @@
-<? php
+<?php
 session_start();
 
+$con mysqli_connect('localhost','root','');
 
-$connecting mysqli_connect('localhost','root','');I
+mysqli_select_db($con, 'registration');
 
+$name $_POST['name'];
+$email $_POST['email'];
+$pass $_POST['password'];
 
-mysqli_select_db($connecting, 'registration');
+$s = " SELECT * FROM user WHERE email = '$email'"
 
+$result mysqli_query($con, $s);
+$num mysqli_num_rows ($result);
 
+if($num==1){
+    echo" Usename Already Taken";
+}
 
-
-
-
-?>
+else{
+        $reg = "insert into user (name , email , password) values ('$name' , '$email', '$pass')";
+        mysqli_query($con, $reg);
+        echo "Sucessful"!;
+}
